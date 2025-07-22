0f32a3fbf691ceca8ee7d488e4624ec71ba81308
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-07-22 12:35:50 +0300

    Part 02

--- Session 6/Program.cs
+++ Session 6/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //-------------------------------------------- Part 02 --------------------------------------------
+            #region Arrays
+
+            #region 1D Array
+            #region Example 01
+            //int[] Numbers;
+            //Numbers = new int[3];
+
+            //Numbers[0] = 1;
+            //Numbers[1] = 2;
+            //Numbers[2] = 3;
+
+            //Console.WriteLine(Numbers[0]);
+            //Console.WriteLine(Numbers[1]);
+            //Console.WriteLine(Numbers[2]);
+            //// 2 * 4 bytes = 8 + 0x0000 = 0x0008
+
+            //Console.WriteLine(Numbers.Length);
+            //Console.WriteLine(Numbers.Rank);
+
+            //for (int i = 0; i < Numbers.Length; i++)
+            //{
+            //    Console.WriteLine(Numbers[i]);
+            //}
+
+            //foreach (int i in Numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            #endregion
+
+            #region Array Creation Ways
+            //int[] Numbers01 = new int[3];
+            //int[] Numbers02 = new int[3] { 1, 2, 3 };
+            //int[] Numbers03 = new int[] { 1, 2, 3 };
+            //int[] Numbers04 = { 1, 2, 3 };
+
+            #endregion
+
+            #endregion
+            #region 2D Array[Rectangular]
+
+            #endregion
+            #endregion
+
+
         }
     }
 }
