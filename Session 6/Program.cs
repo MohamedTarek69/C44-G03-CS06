d10b93ccf1aeed99cbf54e3976c4d45dd242c71f
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-07-22 12:51:30 +0300

    Part 03

--- Session 6/Program.cs
+++ Session 6/Program.cs
@@ -45,9 +45,56 @@
             #endregion
 
             #endregion
+            //-------------------------------------------- Part 03 --------------------------------------------
             #region 2D Array[Rectangular]
+            //int[,] Marks = new int[2, 5] { { 100, 90, 30, 50, 40 },
+            //                               { 20, 10, 60, 77, 80 } };
+
+            //Marks[0, 0] = 100;
+            //Marks[0, 1] = 90;
+            //Marks[0, 2] = 40;
+            //Marks[0, 3] = 80;
+            //Marks[0, 4] = 60;
+            //Marks[1, 0] = 99;
+            //Marks[1, 1] = 80;
+            //Marks[1, 2] = 44;
+            //Marks[1, 3] = 40;
+            //Marks[1, 4] = 98;
+
+            //Console.WriteLine(Marks.Length);
+            //Console.WriteLine(Marks.Rank);
+            //Console.WriteLine(Marks.GetLength(0));
+            //Console.WriteLine(Marks.GetLength(1));
+
+            //int[,] Marks = new int[2, 5];
+            
+            //for (int i = 0; i < Marks.GetLength(0); i++)
+            //{
+            //    Console.WriteLine($"The Grade Of Student Number {i + 1}");
+            //    for (int j = 0; j < Marks.GetLength(1); /*j++*/)
+            //    {
+            //        Console.Write($"Subject Number {j + 1} : ");
+            //        //Marks[i,j] = Console.ReadLine();
+            //        bool isParse = int.TryParse(Console.ReadLine(), out Marks[i, j]);
+
+            //        if (isParse)
+            //            j++;
+            //    }
+            //    Console.WriteLine("============================");
+            //}
+            ////Print
+            //for (int i = 0; i < Marks.GetLength(0); i++)
+            //{
+            //    Console.WriteLine($"The Grade Of Student Number {i + 1}");
+            //    for (int j = 0; j < Marks.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine($"Subject Number {j + 1} : {Marks[i,j]}");
+            //    }
+            //    Console.WriteLine("============================");
+            //}
 
             #endregion
+
             #endregion
 
 
